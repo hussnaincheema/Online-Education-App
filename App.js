855217b8bb886b1b81090bab9063026d6ae98675
Hussnain Cheema <76845698+hussnaincheema@users.noreply.github.com>
2023-09-20 03:04:38 -0400

    Added Files

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AuthStack from './app/Navigation.js/AuthStack';
+//import Home from './app/Screens/Home';
+
+
+export default function App() {
+  return (
+    <AuthStack />
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
